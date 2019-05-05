c67c640f164d5931b42e4cdd0130651e5539098a
Lastovicka <j.lastovicka@gmail.com>
2019-05-05 11:24:52 +0200

    novy element

---
+++ vue_projekt/src/components/RedirectUrisField.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="form-group form-row mt-2">
+    <label for="url" class="col-md-2 col-form-label col-form-label-sm">Url</label>
+    <input type="url" id="url" class="col form-control form-control-sm">
+    <button type="button" class="btn btn-sm btn-success col-md-2" v-on:click="onAdd">Add</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      onAdd: function() {
+        window.alert("Ahoj!!!!");
+      }
+    }
+  }
+</script>
--- vue_projekt/src/components/RegistrationForm.vue
+++ vue_projekt/src/components/RegistrationForm.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="row">
-    <form class="offset-md-4 col-4 bg-success"> 
-      <div class="form-group">
-        <input type="text" class="form-controll form-controll-sm" v-bind:value="message">
-      </div>
+    <form class="offset-md-4 col-4 bg-light border rounded"> 
+      <RedirectUrisField/>
     </form>
   </div>
 </template>
 
 <script>
+  import RedirectUrisField from './RedirectUrisField.vue';
   export default {
     data: function() {
       return {
         message: 'Ahoj Honzo...'
       }
+    },
+    components: {
+      RedirectUrisField
     }
   }
 </script>
